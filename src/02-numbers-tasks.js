b6001b4106d396a8bcac0f68afb890ad1b40b21e
Viktoryia-Kasabutskaya <viktoryia.kasabutskaya@gmail.com>
2023-10-05 13:45:17 +0200

    fix: solve eslint

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -197,7 +197,7 @@ function roundToPowerOfTen(num, pow) {
  *   17 => true
  */
 function isPrime(n) {
-  for (let i = 2; i < n; i++) {
+  for (let i = 2; i < n; i += 1) {
     if (!(n % i)) return false;
   }
   return true;
